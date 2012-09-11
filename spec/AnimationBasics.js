6a4d92128bc217a18671aa4d95d99206914846b6
Allen Lyons <allen.lyons@gmail.com>
2012-09-11 18:24:31 -0400

    Basic Jasmine working

---
+++ spec/AnimationBasics.js
@@ -0,0 +1,12 @@
+describe("Basic Animations", function() {
+  
+  var animation = new Animation();
+  
+  beforeEach(function() {
+    
+  });
+  
+  it("contains spec with an expectation", function() {
+    expect(animation).toBe(true);
+  });
+});
