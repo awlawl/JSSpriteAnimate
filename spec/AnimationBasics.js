22f0371583a097426bf0b254ea728b5b58255b29
Allen Lyons <allen.lyons@gmail.com>
2012-09-13 23:09:01 -0400

    Basic animation is working now
    need to fix unit tests

--- spec/AnimationBasics.js
+++ spec/AnimationBasics.js
@@ -4,10 +4,18 @@ describe("Basic Animations", function() {
   var animation;
   var frameSize = {X:64,Y:64};
   var testDuration = 1000;
-  var testFrameCount = 5;
+  var testFrameCount = 4;
+  var canvas = document.createElement("canvas");
+  var context = canvas.getContext('2d');
+  canvas.height="100",
+  canvas.width="100";
+  document.all.appendChild(canvas);
   
   beforeEach(function() {
     animation = new Animation(testImage, frameSize, testDuration, testFrameCount);
+	
+	
+	
   });
   
   it("must exist", function() {
@@ -48,5 +56,17 @@ describe("Basic Animations", function() {
   
   it("must track the correct number of frames", function() {
 	expect(animation.frameCount).toBe(5);
+  });
+  
+  it("must have a drawFrame function", function() {
+	expect(animation.drawFrame).toBeDefined();
+  });
+  
+  it("must draw the first frame", function() {
+	animation.drawFrame(0,{X:0,Y:0},context);
+	var pixelData = context.getImageData(0,0,1,1);
+	expect(pixelData[0]).toBe(1);
+	expect(pixelData[1]).toBe(0);
+	expect(pixelData[2]).toBe(0);
   });
 });
--- src/Animation.js
+++ src/Animation.js
@@ -1,8 +1,27 @@
-function Animation(imageSource, frameSize, durationMS, frameCount) {
+function Animation(imageSource, frameSize, durationMS, frameCount, finishedLoaded) {
   this.image=new Image();
+  this.image.onload = function () {
+	//alert("hi");
+	finishedLoaded();
+    //animation.drawFrame(0,{X:0,Y:0},context);            
+  };
   this.image.src = imageSource;
   this.frameSize = {X: frameSize.X, Y: frameSize.Y};
   this.durationMS = durationMS;
   this.frameCount = frameCount;
   
+  this.drawFrame = function(timeMS, position, context) {
+	var frameIndex = Math.floor(((timeMS%durationMS)/durationMS)*this.frameCount)*frameSize.X;
+	context.drawImage(this.image,
+            frameIndex,
+			0,
+            frameSize.X,
+            frameSize.Y,
+            position.X,
+			position.Y,
+            frameSize.X,
+            frameSize.Y);
+		
+		//	context.drawImage(this.image,0,0);
+  }
 }
